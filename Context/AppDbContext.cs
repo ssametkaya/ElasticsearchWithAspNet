056febb48ba229746388831ae22471a380eba15c
Süleyman Samet Kaya <ssametkaya52@gmail.com>
2023-10-16 20:27:08 +0300

    Initial commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ElasticsearchWithAspNet.Context
+{
+    public sealed class AppDbContext:DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-94U96R1;Database=TestDb;Integrated Security=true;");
+        }
+
+        public DbSet<Travel> Travels { get; set; }
+    }
+
+    public sealed class Travel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
